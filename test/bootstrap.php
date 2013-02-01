2be93e60b7434a96907ef20e3c88aa602a6db38d
Lonny Kapelushnik <lonnyk@gmail.com>
2013-02-01 14:18:59 -0500

    Removed unused autoloading config

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,9 +1,3 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-/*
-use Composer\Autoload\ClassLoader;
-
-$ClassLoader = new ClassLoader;
-$ClassLoader->add('Requests', __DIR__.'/../lib/');
-$ClassLoader->register();*/
